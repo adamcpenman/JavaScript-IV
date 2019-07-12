57b20c94f3e56fd7590201fd9482bdba3645bba1
Your Name <you@example.com>
2019-07-11 20:12:10 -0500

    finished the lambda-classes though struggled quite a bit, trying to work on the stretch goals. I always forget to push through my work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,110 @@
 // CODE here for your Lambda Classes
+
+class Person { 
+    constructor(baseclass) {
+        this.name = baseclass.name,
+        this.age = baseclass.age,
+        this.location = baseclass.location;
+    }
+    speak () {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(teacher) {
+        super(teacher);
+        this.speciality = teacher.speciality,
+        this.favLanguage = teacher.favLanguage,
+        this.catchPhrase = teacher.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+       return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person {
+    constructor(scholar) { 
+        super(scholar);
+        this.previousBackground = scholar.previousBackground,
+        this.className = scholar.className,
+        this.favSubjects = scholar.favSubjects;
+    }
+    listsSubjects () { 
+       return `${this.favSubjects}`;
+    }
+    PRAssignment (student, subject) {
+      return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge (student, subject) {
+      return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+    grade () {
+
+    }
+}
+
+class TeamLeads extends Instructor {
+    constructor(leaders) {
+        super(leaders);
+        this.gradClassName = leaders.gradClassName,
+        this.favInstructor = leaders.favInstructor; 
+    }
+    standUp (channel) {
+    return  `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
+       
+    }
+    debugsCode (student, subject) {
+      return  `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
+const adam = new Person ({
+    name: "Adam",
+    location: 'Chicago',
+    age: 30,
+    speciality: "CSS",
+    favLanguage: "Javascript", 
+    catchPhrase: "Throw some glitter, make it rain",
+    previousBackground: "Bartender",
+    className: "WEBPT8",
+    favSubjects: ["HTML, CSS, JavaScript"],
+    gradClassName: "CS1",
+    favInstructor: "Dan",
+    subject: "HTML"
+})
+
+const nate = new Instructor ({
+    speciality: "CSS",
+    favLanguage: "Javascript", 
+    catchPhrase: "Throw some glitter, make it code",
+})
+
+const nora = new Student ({
+    name: "Nora",
+    catchPhrase: "Throw some glitter, make it rain",
+    previousBackground: "Bartender",
+    className: "WEBPT8",
+    favSubjects: ["HTML, CSS, JavaScript"]
+})
+
+const leslie = new TeamLeads ({
+    name: "leslie",
+    gradClassName: "Web8",
+    favInstructor: "Dan",
+    subject: "React"
+
+})
+
+console.log(adam);
+console.log(adam.speak());
+console.log(nate.demo("html"));
+console.log(nate.grade(nora, "HTML"));
+console.log(nora.listsSubjects());
+console.log(nora.PRAssignment(nora, "Javascript"));
+console.log(nora.sprintChallenge(nora, "a mental breakdown"));
+console.log(leslie.standUp("web_pt8"));
+console.log(leslie.debugsCode(nora, "REACT"));
